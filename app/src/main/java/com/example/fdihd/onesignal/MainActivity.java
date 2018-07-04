dff410e42229c017bb80a5041f2c7d809961b81d
Cristea Stelica <ghostrayder9321@gmail.com>
2018-07-04 17:54:20 +0300

    Initial commit

---
+++ app/src/main/java/com/example/fdihd/onesignal/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.fdihd.onesignal;
+
+import com.onesignal.OneSignal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        OneSignal.startInit(this)
+                .inFocusDisplaying(OneSignal.OSInFocusDisplayOption.Notification)
+                .unsubscribeWhenNotificationsAreDisabled(true)
+                .init();
+    }
+
+
+}
+
+
